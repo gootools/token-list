6eb687308240de094b38ee0b3a2fb86fb0f75078
John Rees <john@bitsushi.com>
2021-12-30 12:32:30 +0000

    actions work

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,6 +1,7 @@
-const { tokens } = require("../solana.tokenlist.json");
+require("isomorphic-fetch");
 const { stringify } = require("yaml");
 const { writeFile, mkdir } = require("fs/promises");
+const { join } = require("path");
 
 const CLUSTERS = {
   101: "mainnet-beta",
@@ -8,9 +9,19 @@ const CLUSTERS = {
   103: "devnet",
 };
 
-const ob = {};
+/**
+ * fetches all tokens from the original repo
+ */
+const sync = async () => {
+  const req = await fetch(
+    "https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json"
+  );
+  const { tags, tokens } = await req.json();
 
-const go = async () => {
+  const ob = {};
+
+  const allTags = new Set();
+
   tokens
     // load mainnet tokens first
     .sort((a, b) => a.chainId - b.chainId)
@@ -26,21 +37,45 @@ const go = async () => {
 
       if (tags && tags.length > 0) {
         rest.tags = tags.sort(([a], [b]) => a.localeCompare(b));
+
+        rest.tags.forEach((tag) => allTags.add(tag));
       }
 
       ob[address] = rest;
     });
 
-  await Promise.all(
-    Object.entries(ob).map(async ([address, { clusters, ...rest }]) => {
+  await Promise.all([
+    writeFile(
+      join(__dirname, "../tags.json"),
+      JSON.stringify(
+        Object.entries(tags)
+          .sort(([a], [b]) => a.localeCompare(b))
+          .reduce(
+            (acc, [k, v]) => {
+              acc.original[k] = v;
+              return acc;
+            },
+            {
+              original: {},
+              extra: [...allTags]
+                .sort((a, b) => a.localeCompare(b))
+                .filter((t) => !tags[t]),
+            }
+          ),
+        null,
+        2
+      )
+    ),
+
+    ...Object.entries(ob).map(async ([address, { clusters, ...rest }]) => {
       const dir = `tokens/${address[0].toLowerCase()}`;
       await mkdir(dir, { recursive: true });
       if (clusters.toString() !== "mainnet-beta") {
         rest.clusters = clusters;
       }
       await writeFile(`${dir}/${address}.yml`, stringify(rest));
-    })
-  );
+    }),
+  ]);
 };
 
-go();
+sync();
