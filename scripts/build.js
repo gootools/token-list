16c3085c81bf9caa28a5545c9b1df7cce869c09b
John Rees <john@bitsushi.com>
2021-12-30 13:02:59 +0000

    index.html

--- scripts/build.js
+++ scripts/build.js
@@ -9,8 +9,17 @@ const { join } = require("path");
 
   await mkdir(dir, { recursive: true });
 
-  await Promise.all(
-    output.flatMap((item) =>
+  await Promise.all([
+    writeFile(
+      join(__dirname, `../out/index.html`),
+      output
+        .flatMap(({ cluster }) => [
+          `<a href="v1/${cluster}.json">${cluster}</a>`,
+          `<a href="v1/${cluster}.min.json">${cluster} (minimized)</a>`,
+        ])
+        .join("<br />\n")
+    ),
+    ...output.flatMap((item) =>
       Promise.all([
         writeFile(
           join(dir, `${item.cluster}.json`),
@@ -18,6 +27,6 @@ const { join } = require("path");
         ),
         writeFile(join(dir, `${item.cluster}.min.json`), JSON.stringify(item)),
       ])
-    )
-  );
+    ),
+  ]);
 })();
